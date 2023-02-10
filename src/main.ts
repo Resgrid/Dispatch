212db0ff892d070e888f19a342efa1e73e198a45
Shawn Jackson <shawn@resgrid.com>
2023-02-10 11:49:00 -0800

    #8669gwcn1 Minor fix for Sentry

--- src/main.ts
+++ src/main.ts
@@ -9,18 +9,37 @@ if (environment.production) {
   enableProdMode();
 }
 
-Sentry.init({
-  dsn: environment.loggingKey,
-  release: environment.version,
-  integrations: [
-    new Integrations.BrowserTracing({
-      tracingOrigins: ["localhost", "https://api.resgrid.com"],
-      routingInstrumentation: Sentry.routingInstrumentation,
-    }),
-  ],
+const serverErrorsRegex = new RegExp(
+  `500 Internal Server Error|401 Unauthorized|403 Forbidden|404 Not Found|502 Bad Gateway|503 Service Unavailable`,
+  'mi'
+);
 
-  tracesSampleRate: 0.1,
-});
+if (environment.loggingKey && environment.loggingKey !== 'LOGGINGKEY') {
+  Sentry.init({
+    dsn: environment.loggingKey,
+    release: environment.version,
+    environment: environment.production ? 'prod' : 'dev',
+    // We ignore Server Errors. We have to define here since Angular
+    // http client uses setTimeout to detect http call progress.
+    // And when the call fails, it throws an exception inside that timeout
+    // that bubbles up higher to the main Angular's error handler.
+    ignoreErrors: [serverErrorsRegex],
+    integrations: [
+      // Registers and configures the Tracing integration,
+      // which automatically instruments your application to monitor its
+      // performance, including custom Angular routing instrumentation
+      new Integrations.BrowserTracing({
+        tracingOrigins: ['localhost', 'https://api.resgrid.com/api', 'https://dispatch.resgrid.com'],
+        routingInstrumentation: Sentry.routingInstrumentation,
+      }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0.1,
+  });
+}
 
 enableProdMode();
 platformBrowserDynamic()
